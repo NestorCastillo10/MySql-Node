537d358a3008ca84514933463657e0abaca3c215
Ivica Nikolic <ivicanklc@gmail.com>
2020-12-07 12:26:58 +0100

    add routes

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 const express = require("express");
 const app = express();
 
+//routes
+app.get("/", (req, res) => {
+  res.send("Landing page!");
+});
+
+app.get("/addelf", (req, res) => {
+  res.send("add elf");
+});
+
 //port
 app.listen(3050, () => {
   console.log("Hohoho!! App listening on port 3050");
