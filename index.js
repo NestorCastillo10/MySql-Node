4cbbdb813a15ebf99f342700c76c3638dc12c8a9
Ivica Nikolic <ivicanklc@gmail.com>
2020-12-07 12:37:12 +0100

    integrating mysql

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // import  packages
 const express = require("express");
 const app = express();
+const mySql = require("mysql");
+
+const myPassword = "pass";
+
+//port
+app.listen(3050, () => {
+  console.log("Hohoho!! App listening on port 3050");
+});
 
 //routes
 app.get("/", (req, res) => {
@@ -11,7 +19,10 @@ app.get("/addelf", (req, res) => {
   res.send("add elf");
 });
 
-//port
-app.listen(3050, () => {
-  console.log("Hohoho!! App listening on port 3050");
+
+//
+const db = mySql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: myPassword,
 });
